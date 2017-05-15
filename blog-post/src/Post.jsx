e11dcd3c68c2ae4b2f686b3c15f31a3eccba30ff
jnhustin <jnhustin@gmail.com>
2017-05-15 11:18:00 -0700

    fragmented comments into a component

--- blog-post/src/Post.jsx
+++ blog-post/src/Post.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Comment from './Comment.jsx';
 
 class Post extends Component {
 
@@ -8,7 +9,10 @@ class Post extends Component {
         <h1>{this.props.title}</h1>
         <h3>by {this.props.author}</h3>
         <p>{this.props.body}</p>
-        <p>{this.props.comments[0]}</p>
+        <h3><u>Comments</u></h3>
+        { this.props.comments.map( (comment, idx) => 
+            <Comment key={idx} body={comment} />
+        )}
       </div>
     );
   }
