2ee5ffd0b6bcfed47e45be0e69baf13525f0eabf
jnhustin <jnhustin@gmail.com>
2017-05-16 13:16:40 -0700

    blog, added user input to modify state

--- blog-post/src/components/Post.jsx
+++ blog-post/src/components/Post.jsx
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import Comment from './Comment.jsx';
 import Author from './Author.jsx';
-class Post extends Component {
 
+class Post extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      body: this.props.body
+    , formInput: ''
+    };
+  }
+  handleSubmit() {
+    this.setState({
+      body: this.state.formInput
+    , formInput: ''
+    });
+  }
+  handleFormInput(e) {
+    this.setState({
+      formInput: e.target.value
+    });
+  }
   render() {
     let authors = this.props.allAuthors.map( (author, idx) =>
       <Author author={author} key={idx} />
@@ -11,13 +30,23 @@ class Post extends Component {
     return(
       <div>
         <h1>{this.props.title}</h1>
-        <p>{this.props.body}</p>
+        <p>{this.state.body}</p>
         <h3>Written By</h3>
         {authors}
         <h3><u>Comments</u></h3>
         { this.props.comments.map( (comment, idx) => 
           <Comment key={idx} body={comment} />
         )}
+        <input 
+          type="text"
+          value={this.state.formInput}
+          onChange={ (e) => this.handleFormInput(e) }
+        />
+        <button 
+          onClick={ (e) => this.handleSubmit() }
+        >
+          edit body
+        </button>
       </div>
     );
   }
