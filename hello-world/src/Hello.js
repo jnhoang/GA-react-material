e38c99cfa764b407e6dbbd2acbefed7214028575
jnhustin <jnhustin@gmail.com>
2017-05-16 12:22:01 -0700

    added state functionality to hello world app

--- hello-world/src/Hello.js
+++ hello-world/src/Hello.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 
 class Hello extends Component {
+  constructor() {
+    super();
+    this.state = {
+      moodPoints: 1
+    }
+  }
+  increaseMood() {
+    this.setState({
+      moodPoints: (this.state.moodPoints + 1) % 11
+    });
+  }
+
   render() {
     let animalList = this.props.animals.join(', ');
     
@@ -9,6 +21,13 @@ class Hello extends Component {
         <h1>Hello {this.props.name}</h1>
         <p>Your age is {this.props.age}</p>
         <p>You love: {animalList}</p>
+        <p>On a scale of 1-10</p>
+        <p>You are this happy: {this.state.moodPoints}</p>
+        <button 
+          onClick={ (e) => this.increaseMood() }
+        >
+          Cheer up!
+        </button>
       </div>
     );
   }
